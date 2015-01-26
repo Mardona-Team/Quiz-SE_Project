3201eccaf7d8499afa094209f9b08670ec55a773
ali-mohamed <ali.moh.fatouh@gmail.com>
2015-01-26 13:34:16 +0200

    Issue #14 Add Validations for the group

--- Quiz_SE/app/models/group.rb
+++ Quiz_SE/app/models/group.rb
@@ -1,4 +1,14 @@
 class Group < ActiveRecord::Base
+	
+	NAME_REGEX = /\A[a-zA-Z]+\z/
+
+	#Relationships
 	has_many :memberships
 	belongs_to :instructor, class_name: 'User', foreign_key: 'instructor_id'
+	has_many :quizzes
+
+	#Validations
+	validates_presence_of :year
+	validates_presence_of :subject
+	validates :group_name, presence: true, uniqueness: true, format: NAME_REGEX, length: { minimum: 4, maximum: 12}
 end
