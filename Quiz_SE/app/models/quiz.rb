526e624844ccd82e65bfcf95c2aff8f69c8ff80c
ali-mohamed <ali.moh.fatouh@gmail.com>
2015-01-26 13:36:22 +0200

    Issue #6 Add Validations for the quiz

--- Quiz_SE/app/models/quiz.rb
+++ Quiz_SE/app/models/quiz.rb
@@ -1,5 +1,14 @@
 class Quiz < ActiveRecord::Base
+	
+	#Relatioships
 	belongs_to :instructor, class_name: 'User', foreign_key: 'instructor_id'
 	belongs_to :group
 	has_many :questions
+
+	#Validations
+	validates_presence_of :title
+	validates_presence_of :subject
+	validates_presence_of :year
+	validates :marks, presence: true, less_than_or_equal_to: 10000, greater_than: 0
+
 end
