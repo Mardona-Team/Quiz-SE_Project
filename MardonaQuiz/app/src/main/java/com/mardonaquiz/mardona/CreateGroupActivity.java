945f83dcaba84802888e2eb56f2d38b4c7ffe19f
ahmedanwer <superahmedawa@gmail.com>
2015-01-30 01:53:09 +0200

    Grouplist view and create group has been created

--- MardonaQuiz/app/src/main/java/com/mardonaquiz/mardona/CreateGroupActivity.java
+++ MardonaQuiz/app/src/main/java/com/mardonaquiz/mardona/CreateGroupActivity.java
@@ -1,17 +1,52 @@
 package com.mardonaquiz.mardona;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 
 public class CreateGroupActivity extends ActionBarActivity {
 
+    private final static String REGISTER_API_ENDPOINT_URL = "http://es2alny.herokuapp.com/api/groups";
+    //TODO Get instructor ID
+    private String instructor_id=null;
+    private String GroupName;
+    private String GroupTitle;
+    private String GroupYear;
+    private String GroupSubj;
+    private String GroupDesc;
+    private SharedPreferences mPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
+        Button b1=(Button) findViewById(R.id.submit);
+        b1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                create_group();
+            }
+        });
     }
 
 
@@ -36,4 +71,90 @@ public class CreateGroupActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    public void create_group(){
+
+        GroupTitle=((EditText) findViewById(R.id.Group_title)).getText().toString();
+        GroupYear=((EditText) findViewById(R.id.Group_year)).getText().toString();
+        GroupSubj=((EditText) findViewById(R.id.Group_subject)).getText().toString();
+        GroupDesc=((EditText) findViewById(R.id.Group_desc)).getText().toString();
+        //TODO get a uniq group name
+        GroupName=GroupTitle;
+
+
+        if(GroupTitle.length()==0||GroupYear.length()==0||GroupSubj.length()==0)
+        {
+            Toast.makeText(this, getString(R.string.empty_field_error), Toast.LENGTH_LONG).show();
+        }
+        else{
+            AddGroupToAPI addGrouptoapi = new AddGroupToAPI();
+            addGrouptoapi.execute(REGISTER_API_ENDPOINT_URL);
+        }
+    }
+    private class AddGroupToAPI extends AsyncTask<String,Void,JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(String... urls) {
+
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost(urls[0]);
+            String response = null;
+
+            JSONObject groupObj = new JSONObject();
+            JSONObject holder = new JSONObject();
+            JSONObject json = new JSONObject();
+            try{
+                try {
+                    groupObj.put("instructor_id",instructor_id);
+                    groupObj.put("group_name",GroupName);
+                    groupObj.put("title",GroupTitle);
+                    groupObj.put("year",GroupYear);
+                    groupObj.put("subject",GroupSubj);
+                    groupObj.put("description",GroupDesc);
+                    StringEntity se = new StringEntity(groupObj.toString());
+                    post.setEntity(se);
+
+                    post.setHeader("Accept", "application/json");
+                    post.setHeader("Content-Type", "application/json");
+                    ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                    response = client.execute(post, responseHandler);
+                    json = new JSONObject(response);
+
+
+                }catch (HttpResponseException e) {
+                    e.printStackTrace();
+                    Log.e("ClientProtocol", "" + e);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("IO", "" + e);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e("JSON", "" + e);
+            }
+
+
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+
+
+            try {
+                // launch the HomeActivity and close this one
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+
+
+                Toast.makeText(getApplicationContext(),GroupTitle+ " " +getString(R.string.Group_Created), Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+            } finally {
+                super.onPostExecute(json);
+            }
+
+        }
+    }
+
 }
--- MardonaQuiz/app/src/main/java/com/mardonaquiz/mardona/GroupListActivity.java
+++ MardonaQuiz/app/src/main/java/com/mardonaquiz/mardona/GroupListActivity.java
@@ -1,18 +1,118 @@
 package com.mardonaquiz.mardona;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
-public class GroupListActivity extends ActionBarActivity {
+public class GroupListActivity extends ListActivity {
+
+
+    public static final String TAG = GroupListActivity.class.getSimpleName();
+    protected JSONObject mGroups;
+    protected ProgressBar mProgressBar;
+
+    private final String KEY_group = "groups";
+    private final String KEY_TITLE = "title";
+    private final String KEY_SUBJECT = "subject";
+    private final String KEY_YEAR = "year";
+    private final String KEY_ID = "id";
+    private final String KEY_Desc = "description";
+    private  ArrayList<HashMap<String, String>> allGroups =
+            new ArrayList<HashMap<String, String>>();
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+
+        Intent intent = new Intent(this, ViewGroup.class);
+
+        intent.putExtra(KEY_ID,allGroups.get(position).get(KEY_ID));
+        intent.putExtra(KEY_TITLE,allGroups.get(position).get(KEY_TITLE));
+        intent.putExtra(KEY_SUBJECT,allGroups.get(position).get(KEY_SUBJECT));
+        intent.putExtra(KEY_YEAR,allGroups.get(position).get(KEY_YEAR));
+        intent.putExtra(KEY_Desc,allGroups.get(position).get(KEY_Desc));
+
+        startActivity(intent);
+
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_list);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar1);
+
+
+        if (isNetworkAvailable()) {
+            mProgressBar.setVisibility(View.VISIBLE);
+            GET_Group_list GETGrouplist = new GET_Group_list();
+            GETGrouplist.execute();
+        }
+        else {
+            Toast.makeText(this, "Network is unavailable!", Toast.LENGTH_LONG).show();
+        }
+
+
     }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+
+        return isAvailable;
+    }
+
+
+
+    public void openCreateGroup(View view) {
+        //TODO replace this function with a function calling the group creation activity
+
+    }
+
+    public void openViewGroup(View view) {
+        //TODO replace this function with a function calling the group creation activity
+        Intent intent = new Intent(this, ViewGroup.class);
+        startActivity(intent);
+    }
+
 
 
     @Override
@@ -36,4 +136,117 @@ public class GroupListActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void handleResponse() {
+        mProgressBar.setVisibility(View.INVISIBLE);
+
+        if (mGroups == null) {
+            updateDisplayForError();
+        }
+        else {
+            try {
+                JSONArray Mgroups = mGroups.getJSONArray(KEY_group);
+
+                for (int i = 0; i < Mgroups.length(); i++) {
+                    JSONObject post = Mgroups.getJSONObject(i);
+
+
+                    String ID = post.getString(KEY_ID);
+                    String title = post.getString(KEY_TITLE);
+                    String Subject = post.getString(KEY_SUBJECT);
+                    String year = post.getString(KEY_YEAR);
+                    String description = post.getString(KEY_Desc);
+
+
+
+                    HashMap<String, String> mygroup = new HashMap<String, String>();
+
+                    mygroup.put(KEY_ID, ID);
+                    mygroup.put(KEY_TITLE, title);
+                    mygroup.put(KEY_SUBJECT, Subject);
+                    mygroup.put(KEY_YEAR, year);
+                    mygroup.put(KEY_Desc, description);
+
+
+
+                    allGroups.add(mygroup);
+                }
+
+                String[] keys = { KEY_TITLE, KEY_SUBJECT};
+                int[] ids = { android.R.id.text1, android.R.id.text2 };
+                SimpleAdapter adapter = new SimpleAdapter(this, allGroups,
+                        android.R.layout.simple_list_item_2,
+                        keys, ids);
+
+                setListAdapter(adapter);
+            }
+            catch (JSONException e) {
+                Log.e(TAG, "Exception caught!", e);
+            }
+        }
+    }
+
+    private void updateDisplayForError() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("connection error");
+        builder.setMessage("there is a connection error");
+        builder.setPositiveButton(android.R.string.ok, null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        TextView emptyTextView = (TextView) getListView().getEmptyView();
+        emptyTextView.setText("no items to be displayed");
+    }
+
+    private class GET_Group_list extends AsyncTask<Object, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(Object... arg0) {
+            int responseCode = -1;
+            JSONObject jsonResponse = null;
+
+            try {
+                URL blogFeedUrl = new URL("http://es2alny.herokuapp.com/api/groups");
+                HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+                connection.connect();
+
+                responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    InputStream inputStream = connection.getInputStream();
+                    Reader reader = new InputStreamReader(inputStream);
+                    int contentLength = connection.getContentLength();
+                    char[] charArray = new char[contentLength];
+                    reader.read(charArray);
+                    String responseData = new String(charArray);
+
+                    JSONArray j1=new JSONArray(responseData);
+                   JSONObject j2=new JSONObject();
+                    j2.put(KEY_group,j1);
+                    jsonResponse = j2;
+                }
+                else {
+                    Log.i(TAG, "Unsuccessful HTTP Response Code: " + responseCode);
+                }
+            }
+            catch (MalformedURLException e) {
+                Log.e(TAG, "Exception caught: ", e);
+            }
+            catch (IOException e) {
+                Log.e(TAG, "Exception caught: ", e);
+            }
+            catch (Exception e) {
+                Log.e(TAG, "Exception caught: ", e);
+            }
+
+            return jsonResponse;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject result) {
+            mGroups = result;
+            handleResponse();
+        }
+
+    }
+
 }
