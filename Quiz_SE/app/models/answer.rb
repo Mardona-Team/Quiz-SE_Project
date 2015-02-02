83d007b851d7d835b3572dbb725d0a2c9e7672d9
ali-mohamed <ali.moh.fatouh@gmail.com>
2015-02-02 22:07:19 +0200

    Issue #6 add creating quiz, adding questions and answers

--- Quiz_SE/app/models/answer.rb
+++ Quiz_SE/app/models/answer.rb
@@ -1,7 +1,10 @@
 class Answer < ActiveRecord::Base
 
+	#Relationships
 	belongs_to :question
  	has_one     :right_answer, class_name: 'Answer',foreign_key: 'right_answer_id',dependent: :destroy
 
-	
+	#Validations
+	validates_presence_of :title
+
 end
--- Quiz_SE/app/models/question.rb
+++ Quiz_SE/app/models/question.rb
@@ -1,10 +1,21 @@
 class Question < ActiveRecord::Base
 
-  
+	#Relationships
    	has_many    :answers, dependent: :destroy
+   	accepts_nested_attributes_for :answers
    	belongs_to  :right_answer, class_name: 'Answer',foreign_key: 'right_answer_id'
    	belongs_to  :quiz
+   	accepts_nested_attributes_for :right_answer
 
+   	#Callbacks
+   	after_create :add_right_answer_to_answers
+
+   	#Validations
+   	validates_presence_of :title
+
+	def add_right_answer_to_answers
+		self.answers << right_answer
+	end
    	
 
 
