350abd65a7ab6c0aa98a7648137a1fd1d2feba6a
amryassin <amr.yassin.92@gmail.com>
2015-02-08 23:30:40 +0200

    version1 join group

--- MardonaQuiz/app/src/main/java/com/mardonaquiz/mardona/ProfileFragment.java
+++ MardonaQuiz/app/src/main/java/com/mardonaquiz/mardona/ProfileFragment.java
@@ -1,10 +1,13 @@
 package com.mardonaquiz.mardona;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ProfileFragment extends Fragment {
 
@@ -15,6 +18,10 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+
+
 
 
 
